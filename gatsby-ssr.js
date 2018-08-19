825a37dbe62c19f5c5905be41176e02cc05fa644
utkarsh <theutkarsh@outlook.com>
2018-08-19 23:02:27 +0530

    code cleanup

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,59 +1,7 @@
-import { renderToString } from 'react-dom/server'
-import { SheetsRegistry } from 'react-jss/lib/jss';
-import JssProvider from 'react-jss/lib/JssProvider';
-import {
-  MuiThemeProvider,
-  createMuiTheme,
-  createGenerateClassName,
-} from '@material-ui/core/styles';
-import green from '@material-ui/core/colors/green';
-import red from '@material-ui/core/colors/red';
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
 
-function handleRender(req, res) {
-  // Create a sheetsRegistry instance.
-  const sheetsRegistry = new SheetsRegistry();
-
-  // Create a sheetsManager instance.
-  const sheetsManager = new Map();
-
-  // Create a theme instance.
-  const theme = createMuiTheme({
-    palette: {
-      primary: green,
-      accent: red,
-      type: 'light',
-    },
-  });
-
-  // Create a new class name generator.
-  const generateClassName = createGenerateClassName();
-
-  // Render the component to a string.
-  const html = renderToString(
-    <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
-      <MuiThemeProvider theme={theme} sheetsManager={sheetsManager}>
-        <App />
-      </MuiThemeProvider>
-    </JssProvider>
-  )
-
-  // Grab the CSS from our sheetsRegistry.
-  const css = sheetsRegistry.toString()
-
-  // Send the rendered page back to the client.
-  res.send(renderFullPage(html, css))
-}
-function renderFullPage(html, css) {
-  return `
-    <!doctype html>
-    <html>
-      <head>
-        <title>Material-UI</title>
-      </head>
-      <body>
-        <div id="root">${html}</div>
-        <style id="jss-server-side">${css}</style>
-      </body>
-    </html>
-  `;
-}
+// You can delete this file if you're not using it
